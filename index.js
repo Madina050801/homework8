82bc0072ebb9b67d3a7da38e60c02346fcca89b6
Madina050801 <madinaabduraufova@gmail.com>
2021-11-10 14:38:15 +0500

    exe3_done

---
+++ index.js
@@ -0,0 +1,56 @@
+// 1. Object kalitlari va qiymatlarini tengligini tekshirish
+// const data = { a: 1, b: 1 };
+// const data2 = { a: 1, b: 1 };
+// const data3 = { a: 1, b: 2 };
+
+// areEqual( data, data2 )  => true
+// areEqual( data, data3 )  => false
+
+// function objectEquals(obj1, obj2) {
+//     for (var i in obj1) {
+//         if (obj1.hasOwnProperty(i)) {
+//             if (!obj2.hasOwnProperty(i)) return false;
+//             if (obj1[i] != obj2[i]) return false;
+//         }
+//     }
+//     for (var i in obj2) {
+//         if (obj2.hasOwnProperty(i)) {
+//             if (!obj1.hasOwnProperty(i)) return false;
+//             if (obj1[i] != obj2[i]) return false;
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(objectEquals(data, data3));
+
+// 2. Object ichidagi Objectni qiymatlari mavjudligini tekshirish
+
+// const data = { a: { b: undefined } };
+// const data2 = { a: { b: 'it does exits' } };
+// const data3 = { a: { b: 'it does exits', innerKey: null } };
+
+// nestedValueExists(data)  => false
+// nestedValueExists(data2)  => true
+// nestedValueExists(data3)  => false
+
+
+
+// 3. Object qiymatlari va kalitlari bir xillari borligini tekshirish
+
+const data = { a: 1, b: 2 };
+const data2 = { c: 1, b: 2 };
+const data3 = { c: 3, b: 5 };
+
+// checkSame( data, data2 )  => true
+// checkSame( data2, data3 )  => false
+
+function objectEquals(obj1, obj2) {
+    for (var i in obj1) {
+        for( var j in obj2) {
+            if(obj1[i]==obj2[j]) return true
+        }
+    }
+    return false;
+}
+console.log(objectEquals(data3, data2));
